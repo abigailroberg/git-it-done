381dad8d93494273bb6bc555b6a2c03a20e0bebb
Abby Roberg <abbyroberg@gmail.com>
2021-07-26 14:15:46 -0500

    notify user of issues not displayed

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,7 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
-    console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
 
@@ -10,6 +10,11 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                //check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             })
         }
         else {
@@ -53,4 +58,15 @@ var displayIssues = function(issues) {
 
         issueContainerEl.appendChild(issueEl);
     }
+}
+
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
 }
